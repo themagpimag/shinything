31ee92dafcf8cc34786043cb8ba2ca55c349cc31
William H. Bell <W.Bell@cern.ch>
2013-01-28 15:46:16 +0100

    Adding code

--- c-cave/article005/src/ex025.c
+++ c-cave/article005/src/ex025.c
@@ -1,33 +1,11 @@
+#include <string.h>
 #include <stdio.h>
-
-struct dataPoint {
-  unsigned int timeSeconds;
-  float value;
-};
-
-void printDataPoint(struct dataPoint *dp);
+#include <unistd.h>
 
 int main() {
-  /* Declare a variable of "struct dataPoint" type. */
-  struct dataPoint s;
-
-  /* Assign the struct s some starting values. */
-  s.timeSeconds = 60;
-  s.value = 100.0;
-
-  /* Print the size and memory locations */ 
-  printf("sizeof(s) = %ld\n", sizeof(s));
-  printf("&(s.timeSeconds) = %p\n", &(s.timeSeconds));
-  printf("&(s.value) = %p\n", &(s.value));
-  printf("sizeof(unsigned int) = %ld\n", sizeof(unsigned int));
-  printf("sizeof(float) = %ld\n", sizeof(float)); 
-
-  /* Print the values */
-  printDataPoint(&s);
-
+  char fileName[100], suffix[10]="-data.txt";
+  gethostname(fileName,100);
+  strcat(fileName, suffix); /* Append the suffix to the fileName */
+  printf("%s\n",fileName); 
   return 0;
-}
-
-void printDataPoint(struct dataPoint *dp) {
-  printf("timeSeconds = %d, value = %f\n",dp->timeSeconds,dp->value);
 }
