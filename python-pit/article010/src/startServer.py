c2cb802246c71d1aa7261c3e230ada5786ea92c7
William H. Bell <W.Bell@cern.ch>
2013-02-28 23:56:27 +0100

    Adding corrections

--- python-pit/article010/src/startServer.py
+++ python-pit/article010/src/startServer.py
@@ -1,5 +1,5 @@
 # In the example this script is typed
 from SimpleServer import SimpleServer
 import socket
-server = SimpleServer(socket.getfqdn(), 20000)
+server = SimpleServer("192.168.2.11", 20000)
 server.initialise()
